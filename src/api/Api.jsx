1619752525939d48813f6475f3bf2e030ffd0a56
AntwnhsG <garyfallouAnt@gmail.com>
2024-06-29 16:46:43 +0300

    Replcaed URLs for KONG and Keycloak to match the public ip of Azure VM

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -21,7 +21,7 @@ import axios from "axios";
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:8000/',
+        baseURL: 'http://20.188.44.4:8000/',
         headers: {
             'Content-Type': 'application/json',
         }
--- src/components/login/keycloak.jsx
+++ src/components/login/keycloak.jsx
@@ -1,14 +1,14 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://127.0.0.1:8180/', // Update this URL to your Keycloak instance
-  realm: 'FrontEndRealm', // Update with your realm
-  clientId: 'front-end-app', // Update with your client ID
+  url: 'http://20.188.44.4:8180/', // URL to Keycloak instance
+  realm: 'FrontEndRealm', // Update with realm
+  clientId: 'front-end-app', // Update with client ID
   // Add clientSecret if your client is configured as confidential
   // clientSecret: 'your-client-secret',
   // Add additional initialization options as needed
   // onLoad: 'login-required',
-  // checkLoginIframe: false,
+  //checkLoginIframe: false,
   // promiseType: 'native',
 });
 
